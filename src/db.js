17dfe7fb9ab3d0e8a93f36960da033a0b9e148c0
Sander Ginn <sander@ginn.it>
2019-06-10 11:15:16 +0200

    Some cleanup

--- src/db.js
+++ src/db.js
@@ -1,13 +1,8 @@
 import Knex from 'knex';
 
 var client = new Knex({
-  client: 'mysql2',
-  connection: {
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'tamias_dev'
-  }
+  client: 'pg',
+  connection: 'postgres://tamias:tamias@localhost/tamias_dev'
 });
 
 export default client;
